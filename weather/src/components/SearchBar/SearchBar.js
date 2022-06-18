f116d1636ead1b2604066a74ec76db3f8cfc5469
Joseane Semedo <joseanelmsemedo@gmail.com>
2022-06-18 19:51:20 +0100

    Changed searchLocation to onClick

--- weather/src/components/SearchBar/SearchBar.js
+++ weather/src/components/SearchBar/SearchBar.js
@@ -14,29 +14,29 @@ const SearchBar = (props) => {
   const [location, setLocation] = useState("");
   const [error, setError] = useState("");
 
-  const key = process.env.API_KEY;
-  console.log(key);
+  // const key = process.env.API_KEY;
+  // console.log(key);
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=1deddd564f26d5846aa8a73399dc28d1&lang=eng`;
 
   const searchLocation = (event) => {
-    if (event.key === "Enter") {
-      axios
-        .get(url)
-        .then((response) => {
-          setError("");
-          setData(response.data);
-        })
-        .catch((error) => {
-          setError("Not found");
-        });
-      props.onSearchData(data, error);
-    }
+    // if (event.key === "Enter") {
+    axios
+      .get(url)
+      .then((response) => {
+        setError("");
+        setData(response.data);
+      })
+      .catch((error) => {
+        setError("Not found");
+      });
+    props.onSearchData(data, error);
+    // }
   };
 
   return (
     <div className={`${classes.search_bar} ${classes[globalTheme]}`}>
-      <button className={`${classes.icon_btn}`}>
+      <button className={`${classes.icon_btn}`} onClick={searchLocation}>
         <UilSearch className={`${classes.icon} ${classes[globalTheme]}`} />
       </button>
 
@@ -46,7 +46,7 @@ const SearchBar = (props) => {
         placeholder="Search for location..."
         value={location}
         onChange={(event) => setLocation(event.target.value)}
-        onKeyDown={searchLocation}
+        // onKeyDown={searchLocation}
       ></input>
 
       <button className={`${classes.icon_btn}`}>
