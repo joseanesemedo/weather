6ba8446b53a5b4e5bd5d9fdd28df717008a622ce
Joseane Semedo <joseanelmsemedo@gmail.com>
2022-06-22 15:24:08 +0100

    Added more day time themes

--- weather/src/components/ThemeSwitcher.js
+++ weather/src/components/ThemeSwitcher.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect } from "react";
-// import { formatToLocalTime } from "../services/weatherService";
+import { formatToLocalTime } from "../services/weatherService";
 import {
   DAY_THEME,
   CLOUDY_DAY_THEME,
@@ -13,12 +13,24 @@ import {
 const ThemeSwitcher = ({ weather: { dt, timezone, id, icon } }) => {
   const { setGlobalTheme } = useContext(ThemeContext);
 
-  // let time = formatToLocalTime(dt, timezone, "HH");
+  let time = formatToLocalTime(dt, timezone, "HH");
+  let day_time;
+  if (Number(time) > 6 && Number(time) <= 17) {
+    day_time = "day";
+  } else if (Number(time) > 17 && Number(time) < 19) {
+    day_time = "afternoon";
+  } else {
+    day_time = "night";
+  }
 
   useEffect(() => {
     switch (icon) {
       case "01d":
-        setGlobalTheme(DAY_THEME);
+        if (day_time === "day") {
+          setGlobalTheme(DAY_THEME);
+        } else {
+          setGlobalTheme(AFTERNOON_THEME);
+        }
         break;
 
       case "01n":
@@ -28,7 +40,11 @@ const ThemeSwitcher = ({ weather: { dt, timezone, id, icon } }) => {
       case "02d":
       case "03d":
       case "04d":
-        setGlobalTheme(CLOUDY_DAY_THEME);
+        if (day_time === "day") {
+          setGlobalTheme(CLOUDY_DAY_THEME);
+        } else {
+          setGlobalTheme(AFTERNOON_THEME);
+        }
         break;
 
       case "02n":
@@ -54,14 +70,7 @@ const ThemeSwitcher = ({ weather: { dt, timezone, id, icon } }) => {
         setGlobalTheme(DAY_THEME);
         break;
     }
-    // if (Number(time) > 6 && Number(time) <= 15) {
-    //   setGlobalTheme(DAY_THEME);
-    // } else if (Number(time) > 15 && Number(time) < 19) {
-    //   setGlobalTheme(AFTERNOON_THEME);
-    // } else {
-    //   setGlobalTheme(NIGHT_THEME);
-    // }
-  }, [setGlobalTheme, icon]);
+  }, [setGlobalTheme, icon, day_time]);
 
   return <></>;
 };
